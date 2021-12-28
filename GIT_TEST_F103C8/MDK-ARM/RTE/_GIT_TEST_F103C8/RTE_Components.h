d172f32c36de3634bdcb838245be037c1a914c6d
fzg907312 <515978585@qq.com>
2021-12-28 15:10:10 +0800

    frist code

---
+++ GIT_TEST_F103C8/MDK-ARM/RTE/_GIT_TEST_F103C8/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'GIT_TEST_F103C8' 
+ * Target:  'GIT_TEST_F103C8' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
